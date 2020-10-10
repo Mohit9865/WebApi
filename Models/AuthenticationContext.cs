26829c878c4272fe64b51d82e4b487117974dfa7
Rajesh <rajesh.maurya033250@gmail.com>
2020-10-10 16:48:26 +0530

    another commit

---
+++ Models/AuthenticationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApi2.Models
+{
+    public class AuthenticationContext : IdentityDbContext
+    {
+        public AuthenticationContext(DbContextOptions option):base(option)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+    }
+}
---
+++ Models/ExpirePaymentDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+public class ExpirePaymentDetail
+{
+    [Column(TypeName = "Int")]
+    public int PMID { get; set; }
+    [Column(TypeName = "nvarchar(100)")]
+    public string CardOwnerName { get; set; }
+    [Required]
+    [Column(TypeName = "varchar(16)")]
+    public string CardNumber { get; set; }
+    [Required]
+    [Column(TypeName = "varchar(5)")]
+    public string ExpirationDate { get; set; }
+    [Required]
+    [Column(TypeName = "varchar(3)")]
+    public string CVV { get; set; }
+}
